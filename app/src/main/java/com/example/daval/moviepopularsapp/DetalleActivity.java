4a8c6d37e571fc65ba6fa335c8476025721af625
Daniel Valencia <dfvalenciav@gmail.com>
2021-04-25 15:05:39 -0500

    02-1 Activity_detalle

---
+++ app/src/main/java/com/example/daval/moviepopularsapp/DetalleActivity.java
@@ -0,0 +1,14 @@
+package com.example.daval.moviepopularsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+    }
+}
