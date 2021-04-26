089a7cfd4c29f07cf437e1baec609991f328d2a2
Daniel Valencia <dfvalenciav@gmail.com>
2021-04-25 20:03:54 -0500

    09 Ajustes finales

--- app/src/main/java/com/example/daval/moviepopularsapp/DetalleActivity.java
+++ app/src/main/java/com/example/daval/moviepopularsapp/DetalleActivity.java
@@ -1,6 +1,11 @@
 package com.example.daval.moviepopularsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,11 +15,20 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import java.util.List;
+
 public class DetalleActivity extends AppCompatActivity {
+
+    public static String BASE_URL = "https://api.themoviedb.org";
+    public static int PAGE = 1;
+    public static String API_KEY = "3e44a4e77556dfabf904db1a138d30dc";
+    public static String LANGUAGE = "en-US";
+    public static String CATEGORY = "popular";
+    public static String IMAGE = "https://image.tmdb.org/t/p/original";
 
     private TextView movieDetail_Title, movieDetail_Description;
     private ImageView movieDetail_Poster;
-    public static String IMAGE = "https://image.tmdb.org/t/p/original";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,7 @@ public class DetalleActivity extends AppCompatActivity {
         movieDetail_Title = (TextView) findViewById(R.id.movieDetail_Title);
         movieDetail_Description = (TextView) findViewById(R.id.movieDetail_Description);
         movieDetail_Poster = (ImageView) findViewById(R.id.movieDetail_poster);
+
 
         Intent intent = getIntent();
         if(intent.hasExtra("original_Title")){
@@ -38,7 +53,34 @@ public class DetalleActivity extends AppCompatActivity {
                     .into(movieDetail_Poster);
             movieDetail_Description.setText(Description);
         }else {
-            Toast.makeText(this,"NO API DATA" ,Toast.LENGTH_LONG).show();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            ApiInterface MyInterface = retrofit.create(ApiInterface.class);
+
+            Call<MovieResults> call = MyInterface.getMovies(CATEGORY,API_KEY,LANGUAGE,PAGE);
+
+            call.enqueue(new Callback<MovieResults>() {
+                @Override
+                public void onResponse(Call<MovieResults> call, Response<MovieResults> response) {
+                    MovieResults results = response.body();
+                    List<MovieResults.ResultsBean> listOfMovies =  results.getResults();
+                    MovieResults.ResultsBean firstmovie = listOfMovies.get(0);
+                    movieDetail_Title.setText(firstmovie.getTitle());
+                    String ImagePath = firstmovie.getPoster_path();
+                    Glide.with(DetalleActivity.this)
+                            .load(IMAGE+ImagePath)
+                            .into(movieDetail_Poster);
+                    movieDetail_Description.setText(firstmovie.getOverview());
+
+                }
+
+                @Override
+                public void onFailure(Call<MovieResults> call, Throwable t) {
+                    t.printStackTrace();
+                }
+            });
 
         }
     }
